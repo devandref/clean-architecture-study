6625210399bbce1ef0fd790b080f0847af0420de
devandref <yoandredepaula@gmail.com>
2024-06-01 12:30:58 -0300

    init

---
+++ src/main/java/io/github/devandref/clean_architecture_study/CleanArchitectureStudyApplication.java
@@ -0,0 +1,13 @@
+package io.github.devandref.clean_architecture_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devandref/clean_architecture_study/CleanArchitectureStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devandref.clean_architecture_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
